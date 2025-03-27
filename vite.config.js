1cacf5af3cac3550b7218eeb4f4135a96f23cca8
Gokul-ram-j <gokulram221@gmail.com>
2025-03-27 16:27:28 +0530

    config file upd

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "import.meta.env.VITE_OPENROUTER_API_KEY": JSON.stringify(process.env.VITE_OPENROUTER_API_KEY),
+  },
 })
